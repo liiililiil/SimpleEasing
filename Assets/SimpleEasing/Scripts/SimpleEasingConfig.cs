1e4f13b03b9794834d904dcd4eddb8ad49571ef7
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-01-28 04:15:56 +0900

    코드 가독성 개선

--- Assets/SimpleEasing/Scripts/SimpleEasingConfig.cs
+++ Assets/SimpleEasing/Scripts/SimpleEasingConfig.cs
@@ -6,14 +6,17 @@ namespace SimpleEasingConfig
     public class GraphConfig : ScriptableObject 
     {
         // Enable/Disable graph
-        [Space(10), Tooltip("Enable / Disable Graph \n Default : true")]
+        [Space(10)]
+        [Tooltip("Enable / Disable Graph \n Default : true")]
         public bool enableGraph = true;
 
         // Number of samples on the graph
-        [Space(10), Tooltip("Number of samples on the graph \n Default : 100"), Range(10,1000)]
+        [Space(10)]
+        [Tooltip("Number of samples on the graph \n Default : 100"), Range(10,1000)]
         public int sample = 100;
-
-        [Space(10), Tooltip("Minimum Y of the graph \n Default : -0.5"), Range(-1f, 0f)]
+        
+        [Space(10)]
+        [Tooltip("Minimum Y of the graph \n Default : -0.5"), Range(-1f, 0f)]
         public float yMin = -0.5f;
 
         [Tooltip("Maximum Y of the graph \n Default : 1.5"), Range(1f, 2f)]
