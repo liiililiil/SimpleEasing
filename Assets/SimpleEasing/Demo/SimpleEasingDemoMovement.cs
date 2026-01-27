03a7ab3199159aef574c3816d1cf8f5f03589ef0
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-01-28 04:14:43 +0900

    데모 무브먼트 스크립트 구현

--- Assets/SimpleEasing/Demo/SimpleEasingDemoMovement.cs
+++ Assets/SimpleEasing/Demo/SimpleEasingDemoMovement.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+
+// Required to use Easing
 using SimpleEasing;
 
 public class SimpleEasingDemoMovement : MonoBehaviour
@@ -6,8 +8,44 @@ public class SimpleEasingDemoMovement : MonoBehaviour
     [SerializeField]
     private EaseType ease;
 
-    [Space(10), SerializeField]
+    [Space(20)]
+    [SerializeField, Range(1,10)]
     private float duration;
+
+    [Space(20), Header("Position")]
+    [SerializeField]
+    private Vector2 startPosition;
+
+    [SerializeField]
+    private Vector2 targePosition;
+    
+    [Space(20), Header("Rotation")]
+    [SerializeField]
+    private Vector3 startRotation;
+
+    [SerializeField]
+    private Vector3 targetRotation;
+
+    float elapsed;
+
+    private void Update() {
+        elapsed += Time.deltaTime;
+        if(elapsed >= duration) elapsed -= duration;
+
+        float t = elapsed / duration;
+        
+        //Use Easing
+        float easeT = Ease.Easing(t, ease);
+
+        //Position
+        transform.position = Vector2.LerpUnclamped(startPosition, targePosition, easeT); 
+
+        //Rotation
+        Quaternion startQuaternion = Quaternion.Euler(startRotation);
+        Quaternion targetQuaternion = Quaternion.Euler(targetRotation);
+        transform.rotation = Quaternion.LerpUnclamped(startQuaternion, targetQuaternion, easeT);
+
+    }
 
 
 }
