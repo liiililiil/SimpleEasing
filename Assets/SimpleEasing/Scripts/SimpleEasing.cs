53f6bd24b0bf021ef7bb736d6b078b87a0c8eda8
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-01-28 03:21:09 +0900

    대량 호출로 인한 GC Spike를 방지하지 위해 스위치 문 변경 및 코드 리펙토링

--- Assets/SimpleEasing/Scripts/SimpleEasing.cs
+++ Assets/SimpleEasing/Scripts/SimpleEasing.cs
@@ -1,13 +1,10 @@
 using System;
-using UnityEngine;
 using System.Runtime.CompilerServices;
-using System.Collections.Generic;
+
+using UnityEngine;
 
 namespace SimpleEasing
 {
-    /// <summary>
-    /// Easing function Types
-    /// </summary>
     [Serializable]
     public enum EaseType : byte
     {
@@ -24,64 +21,9 @@ namespace SimpleEasing
         Linear
     }
 
-   
-
     public static class Ease
     {
-        private static readonly Dictionary<EaseType, Func<float, float>> easingMap = new Dictionary<EaseType, Func<float, float>>()
-        {
-            { EaseType.InCirc, InCirc },
-            { EaseType.OutCirc, OutCirc },
-            { EaseType.InOutCirc, x => InOut(InCirc, OutCirc, x) },
-            { EaseType.OutInCirc, x => OutIn(OutCirc, InCirc, x) },
-
-            { EaseType.InCubic, InCubic },
-            { EaseType.OutCubic, OutCubic },
-            { EaseType.InOutCubic, x => InOut(InCubic, OutCubic, x) },
-            { EaseType.OutInCubic, x => OutIn(OutCubic, InCubic, x) },
-
-            { EaseType.InBack, InBack },
-            { EaseType.OutBack, OutBack },
-            { EaseType.InOutBack, x => InOut(InBack, OutBack, x) },
-            { EaseType.OutInBack, x => OutIn(OutBack, InBack, x) },
-
-            { EaseType.InQuint, InQuint },
-            { EaseType.OutQuint, OutQuint },
-            { EaseType.InOutQuint, x => InOut(InQuint, OutQuint, x) },
-            { EaseType.OutInQuint, x => OutIn(OutQuint, InQuint, x) },
-
-            { EaseType.InExpo, InExpo },
-            { EaseType.OutExpo, OutExpo },
-            { EaseType.InOutExpo, x => InOut(InExpo, OutExpo, x) },
-            { EaseType.OutInExpo, x => OutIn(OutExpo, InExpo, x) },
-
-            { EaseType.InSine, InSine },
-            { EaseType.OutSine, OutSine },
-            { EaseType.InOutSine, x => InOut(InSine, OutSine, x) },
-            { EaseType.OutInSine, x => OutIn(OutSine, InSine, x) },
-
-            { EaseType.InBounce, InBounce },
-            { EaseType.OutBounce, OutBounce },
-            { EaseType.InOutBounce, x => InOut(InBounce, OutBounce, x) },
-            { EaseType.OutInBounce, x => OutIn(OutBounce, InBounce, x) },
-
-            { EaseType.InQuad, InQuad },
-            { EaseType.OutQuad, OutQuad },
-            { EaseType.InOutQuad, InOutQuad },
-            { EaseType.OutInQuad, x => OutIn(OutQuad, InQuad, x) },
-
-            { EaseType.InQuart, InQuart },
-            { EaseType.OutQuart, OutQuart },
-            { EaseType.InOutQuart, InOutQuart },
-            { EaseType.OutInQuart, x => OutIn(OutQuart, InQuart, x) },
-
-            { EaseType.InElastic, InElastic },
-            { EaseType.OutElastic, OutElastic },
-            { EaseType.InOutElastic, InOutElastic },
-            { EaseType.OutInElastic, x => OutIn(OutElastic, InElastic, x) },
-
-            { EaseType.Linear, x => x }
-        };
+        private const float BackS = 1.70158f;
 
         /// <summary>
         /// Eased the value with the EaseType function.
@@ -92,89 +34,110 @@ namespace SimpleEasing
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float Easing(float value, EaseType easeType)
         {
-            if (easingMap.TryGetValue(easeType, out var func))
-                return func(value);
-            return value;
+            switch (easeType)
+            {
+                case EaseType.InCirc: return InCirc(value);
+                case EaseType.OutCirc: return OutCirc(value);
+                case EaseType.InOutCirc: return InOut(InCirc, OutCirc, value);
+                case EaseType.OutInCirc: return OutIn(OutCirc, InCirc, value);
+
+                case EaseType.InCubic: return InCubic(value);
+                case EaseType.OutCubic: return OutCubic(value);
+                case EaseType.InOutCubic: return InOut(InCubic, OutCubic, value);
+                case EaseType.OutInCubic: return OutIn(OutCubic, InCubic, value);
+
+                case EaseType.InBack: return InBack(value);
+                case EaseType.OutBack: return OutBack(value);
+                case EaseType.InOutBack: return InOut(InBack, OutBack, value);
+                case EaseType.OutInBack: return OutIn(OutBack, InBack, value);
+
+                case EaseType.InQuint: return InQuint(value);
+                case EaseType.OutQuint: return OutQuint(value);
+                case EaseType.InOutQuint: return InOut(InQuint, OutQuint, value);
+                case EaseType.OutInQuint: return OutIn(OutQuint, InQuint, value);
+
+                case EaseType.InExpo: return InExpo(value);
+                case EaseType.OutExpo: return OutExpo(value);
+                case EaseType.InOutExpo: return InOut(InExpo, OutExpo, value);
+                case EaseType.OutInExpo: return OutIn(OutExpo, InExpo, value);
+
+                case EaseType.InSine: return InSine(value);
+                case EaseType.OutSine: return OutSine(value);
+                case EaseType.InOutSine: return InOut(InSine, OutSine, value);
+                case EaseType.OutInSine: return OutIn(OutSine, InSine, value);
+
+                case EaseType.InBounce: return InBounce(value);
+                case EaseType.OutBounce: return OutBounce(value);
+                case EaseType.InOutBounce: return InOut(InBounce, OutBounce, value);
+                case EaseType.OutInBounce: return OutIn(OutBounce, InBounce, value);
+
+                case EaseType.InQuad: return InQuad(value);
+                case EaseType.OutQuad: return OutQuad(value);
+                case EaseType.InOutQuad: return InOutQuad(value);
+                case EaseType.OutInQuad: return OutIn(OutQuad, InQuad, value);
+
+                case EaseType.InQuart: return InQuart(value);
+                case EaseType.OutQuart: return OutQuart(value);
+                case EaseType.InOutQuart: return InOutQuart(value);
+                case EaseType.OutInQuart: return OutIn(OutQuart, InQuart, value);
+
+                case EaseType.InElastic: return InElastic(value);
+                case EaseType.OutElastic: return OutElastic(value);
+                case EaseType.InOutElastic: return InOutElastic(value);
+                case EaseType.OutInElastic: return OutIn(OutElastic, InElastic, value);
+
+                case EaseType.Linear: return value;
+
+                default: return value;
+            }
         }
 
+        #region InOut / OutIn Helpers
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float InOut(Func<float, float> inFunc, Func<float, float> outFunc, float value) => value < 0.5f ? inFunc(value * 2) * 0.5f : outFunc(value * 2 - 1) * 0.5f + 0.5f;
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float OutIn(Func<float, float> outFunc, Func<float, float> inFunc, float value) => value < 0.5f ? outFunc(value * 2) * 0.5f : inFunc(value * 2 - 1) * 0.5f + 0.5f;
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float InCirc(float value) => 1 - Mathf.Sqrt(1 - value * value);
+        private static float InOut(Func<float, float> inFunc, Func<float, float> outFunc, float value) =>
+            value < 0.5f ? inFunc(value * 2) * 0.5f : outFunc(value * 2 - 1) * 0.5f + 0.5f;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float OutCirc(float value) => Mathf.Sqrt(1 - (value - 1) * (value - 1));
+        private static float OutIn(Func<float, float> outFunc, Func<float, float> inFunc, float value) =>
+            value < 0.5f ? outFunc(value * 2) * 0.5f : inFunc(value * 2 - 1) * 0.5f + 0.5f;
+        #endregion
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float InCubic(float value) => value * value * value;
+        #region Base Easing Functions
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] private static float InCirc(float value) => 1 - Mathf.Sqrt(1 - value * value);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] private static float OutCirc(float value) => Mathf.Sqrt(1 - (value - 1) * (value - 1));
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] private static float InCubic(float value) => value * value * value;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] private static float OutCubic(float value) => (value - 1) * (value - 1) * (value - 1) + 1;
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float OutCubic(float value) => (value - 1) * (value - 1) * (value - 1) + 1;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] private static float InBack(float value) => value * value * ((BackS + 1) * value - BackS);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] private static float OutBack(float value) { value--; return value * value * ((BackS + 1) * value + BackS) + 1; }
 
-        private const float BackS = 1.70158f;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] private static float InQuint(float value) => value * value * value * value * value;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] private static float OutQuint(float value) { float inv = 1 - value; return 1 - inv * inv * inv * inv * inv; }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float InBack(float value) => value * value * ((BackS + 1) * value - BackS);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] private static float InExpo(float value) => value == 0 ? 0 : Mathf.Pow(2, 10 * (value - 1));
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] private static float OutExpo(float value) => value == 1 ? 1 : 1 - Mathf.Pow(2, -10 * value);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float OutBack(float value) => (--value) * value * ((BackS + 1) * value + BackS) + 1;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] private static float InSine(float value) => 1 - Mathf.Cos(value * Mathf.PI * 0.5f);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] private static float OutSine(float value) => Mathf.Sin(value * Mathf.PI * 0.5f);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float InQuint(float value) => value * value * value * value * value;
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float OutQuint(float value) => 1 - Mathf.Pow(1 - value, 5);
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float InExpo(float value) => value == 0 ? 0 : Mathf.Pow(2, 10 * (value - 1));
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float OutExpo(float value) => value == 1 ? 1 : 1 - Mathf.Pow(2, -10 * value);
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float InSine(float value) => 1 - Mathf.Cos(value * Mathf.PI / 2);
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float OutSine(float value) => Mathf.Sin(value * Mathf.PI / 2);
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float InBounce(float value) => 1 - OutBounce(1 - value);
-
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] private static float InBounce(float value) => 1 - OutBounce(1 - value);
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static float OutBounce(float value)
         {
             if (value < (1 / 2.75f)) return 7.5625f * value * value;
-            if (value < (2 / 2.75f)) return 7.5625f * (value -= (1.5f / 2.75f)) * value + 0.75f;
-            if (value < (2.5 / 2.75f)) return 7.5625f * (value -= (2.25f / 2.75f)) * value + 0.9375f;
-            return 7.5625f * (value -= (2.625f / 2.75f)) * value + 0.984375f;
+            if (value < (2 / 2.75f)) { value -= (1.5f / 2.75f); return 7.5625f * value * value + 0.75f; }
+            if (value < (2.5f / 2.75f)) { value -= (2.25f / 2.75f); return 7.5625f * value * value + 0.9375f; }
+            value -= (2.625f / 2.75f); return 7.5625f * value * value + 0.984375f;
         }
-        // Quad
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float InQuad(float value) => value * value;
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float OutQuad(float value) => 1 - (1 - value) * (1 - value);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] private static float InQuad(float value) => value * value;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] private static float OutQuad(float value) => 1 - (1 - value) * (1 - value);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] private static float InOutQuad(float value) => value < 0.5f ? 2 * value * value : 1 - Mathf.Pow(-2 * value + 2, 2) * 0.5f;
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float InOutQuad(float value) =>
-            value < 0.5f ? 2 * value * value : 1 - Mathf.Pow(-2 * value + 2, 2) / 2;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] private static float InQuart(float value) => value * value * value * value;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] private static float OutQuart(float value) { float inv = 1 - value; return 1 - inv * inv * inv * inv; }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] private static float InOutQuart(float value) => value < 0.5f ? 8 * value * value * value * value : 1 - Mathf.Pow(-2 * value + 2, 4) * 0.5f;
 
-        // Quart
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float InQuart(float value) => value * value * value * value;
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float OutQuart(float value) => 1 - Mathf.Pow(1 - value, 4);
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static float InOutQuart(float value) =>
-            value < 0.5f ? 8 * value * value * value * value : 1 - Mathf.Pow(-2 * value + 2, 4) / 2;
-
-        // Elastic
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static float InElastic(float value)
         {
@@ -198,9 +161,9 @@ namespace SimpleEasing
             if (value == 1) return 1;
             value *= 2;
             float c = (2 * Mathf.PI) / 4.5f;
-            if (value < 1)
-                return -0.5f * Mathf.Pow(2, 10 * (value - 1)) * Mathf.Sin((value * 10 - 10.75f) * c);
+            if (value < 1) return -0.5f * Mathf.Pow(2, 10 * (value - 1)) * Mathf.Sin((value * 10 - 10.75f) * c);
             return Mathf.Pow(2, -10 * (value - 1)) * Mathf.Sin((value * 10 - 10.75f) * c) * 0.5f + 1;
         }
+        #endregion
     }
 }
