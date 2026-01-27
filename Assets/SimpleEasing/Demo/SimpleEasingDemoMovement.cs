b66248d82389b04060597763f5ed102337d4f606
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-01-28 03:33:46 +0900

    이름 변경

---
+++ Assets/SimpleEasing/Demo/SimpleEasingDemoMovement.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using SimpleEasing;
+
+public class SimpleEasingDemoMovement : MonoBehaviour
+{
+    [SerializeField]
+    private EaseType ease;
+
+    [Space(10), SerializeField]
+    private float duration;
+
+
+}
